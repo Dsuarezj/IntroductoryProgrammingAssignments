e6701023b67ebe3f398e400e00ebe07ceda83cef
Dsuarezj <dsuarez@thoughtworks.com>
2016-06-27 16:15:14 -0500

    add FizzBuzz Exercise

---
+++ src/FizzBuzzExercise.java
@@ -0,0 +1,32 @@
+/**
+ * Created by dsuarez on 6/27/16.
+ */
+public class FizzBuzzExercise {
+
+    public static void main(String[] args) {
+
+        int fizzNumber=3;
+        int buzzNumber =5;
+
+        for (int numberGame = 1; numberGame<=100; numberGame++){
+
+            if(numberGame%fizzNumber == 0 && numberGame%buzzNumber == 0)
+                System.out.println("FizzBuzz");
+            else if (numberGame%fizzNumber == 0)
+                System.out.println("Fizz");
+            else if (numberGame%buzzNumber ==0)
+                System.out.println("Buzz");
+            else
+                System.out.println(numberGame);
+
+
+
+
+        }
+
+
+
+    }
+
+
+}
